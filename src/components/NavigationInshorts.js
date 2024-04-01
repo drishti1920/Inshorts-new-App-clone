d1075892e11e9233aea8b69e504cf5c6252772bb
Drishti Saraf <drishtisaraf0307@gmail.com>
2024-04-01 22:39:19 +0530

    project setup

---
+++ src/components/NavigationInshorts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from "../assessts/logo.png"
+import "./NavigationInshorts.css"
+
+const NavigationInshorts = () => {
+  return (
+    <div className='nav'>
+        <div className='icon'>
+            icon
+        </div>
+        <img src={logo} alt="inshorts" />
+    </div>
+  )
+}
+
+export default NavigationInshorts
